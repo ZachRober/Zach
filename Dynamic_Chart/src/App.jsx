ae7c2d6da10e09dec8b961dafca85102612e84b2
ZachRober <nolethedgehogdie@gmail.com>
2024-12-04 16:53:06 -0500

    import chart

--- Dynamic_Chart/src/App.jsx
+++ Dynamic_Chart/src/App.jsx
@@ -1,8 +1,30 @@
-
+import {Chart} from 'chart.js';
+import React, {useEffect, useRef, useState} from "react";
 import './App.css'
+import ChartComponent from './components/ChartComponent';
 
 function App() {
-  return()
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(()=>{
+    fetch('./downloads/financial_data.json')
+    .then((response)=>{
+      if(!response.ok){
+        throw new Error("Failed to fetch");
+      }
+      return response.json();
+    })
+     .then((data)=>{
+      setChartData(data);
+     })
+     .catch((error)=>{
+      console.error(error);
+     });
+  },[])
+  return(<div>
+    <h1>Charts</h1>
+    <ChartComponent data={chartData}/>
+  </div>)
 }
 
 export default App
---
+++ Dynamic_Chart/src/components/ChartComponent.jsx
@@ -0,0 +1,7 @@
+import React, {useEffect, useRef} from "react";
+import { Chart } from "chart.js";
+
+function ChartComponent({data, type, options}){
+
+}
+export default ChartComponent;
